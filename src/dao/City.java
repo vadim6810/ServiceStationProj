9486cdcc5f0ba92cce28d7c681e2111db4ff57f5
Pavel Bereznichenko <paulo.berezini@mail.ru>
2016-09-13 18:37:25 +0300

    Add files via upload

---
+++ src/dao/City.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by Legat on 8/27/2016.
+ */
+public class City {
+    String name;
+}
---
+++ src/dao/TypeVehicle.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Legat on 8/27/2016.
+ */
+public class TypeVehicle {
+    static Integer id = 0;
+    String txtNameVehicle;
+    ArrayList<Car> cars;
+
+}
